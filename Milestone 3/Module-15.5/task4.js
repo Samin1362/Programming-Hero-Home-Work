1db2b7b15fab245679f61dddff531ae1c8385675
Md. Samin Israk 2021362642 <samin.israk@northsouth.edu>
2025-07-31 16:13:08 +0600

    module 15.5 all task added

---
+++ Milestone 3/Module-15.5/task4.js
@@ -0,0 +1,16 @@
+// 4. Checking if it's an Array
+
+// Instructions:
+
+// Create different variables, each containing either an array or a non-array value.
+
+// Now use isArray to check if each variable is an array.
+
+// Print a message to the console indicating whether each variable is an array or not.
+
+
+const array = [1, 2, 3, 4, 5];
+const notArray = 3;
+
+console.log(Array.isArray(array));
+console.log(Array.isArray(notArray));
---
+++ Milestone 3/Module-15.5/task5.js
@@ -0,0 +1,13 @@
+// 5. Combining Arrays
+
+// Instructions:
+
+// Create two arrays of your choice.
+// Use the concat method to combine the two arrays into a new array.
+// Print both the original arrays and the combined array using console.log().
+
+
+const num1 = [1, 2, 5];
+const num2 = [6, 7, 8, 9];
+
+console.log(num1.concat(num2));

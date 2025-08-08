40c2e0818f1786315173723bbac4e82942afb01b
Md. Samin Israk 2021362642 <samin.israk@northsouth.edu>
2025-08-08 10:16:51 +0600

    module 19 task 1 added

---
+++ Milestone 4/Module-19/task1.js
@@ -0,0 +1,9 @@
+// Task-1
+
+// Take four parameters. Multiply the four numbers and then return the result
+
+function multiply(a, b, c, d){
+  return a*b*c*d;
+}
+
+console.log(multiply(2, 2, 2, 2));
